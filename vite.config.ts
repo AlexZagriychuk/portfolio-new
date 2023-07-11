4fb2948d150c6eeecd7cfc33047bec10e0cac998
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-07-10 22:37:39 -0700

    Add Vite server proxies to access portfolio projects as subfolders on the localhost (similar to what "public/_redirects" Netlify file does on prod)

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    // port: 8080,
+    proxy: {
+      "/portfolio/bikeshop": {
+        target: "https://awesome-bohr-a4110d.netlify.app",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/portfolio\/bikeshop/, ''),
+      },
+      "/portfolio/comfy": {
+        target: "https://zesty-moxie-2cf5ec.netlify.app",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/portfolio\/comfy/, ''),
+      },
+      "/portfolio/simple-fullstack-app": {
+        target: "https://aesthetic-squirrel-fbcde8.netlify.app",
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/portfolio\/simple-fullstack-app/, ''),
+      },
+    }
+  }
 })
