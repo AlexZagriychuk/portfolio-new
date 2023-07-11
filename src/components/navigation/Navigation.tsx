8be690322ee623f56daee59b11d06810887eea35
Alex Zagriychuk <alexzagriychuk@gmail.com>
2023-07-11 01:34:18 -0700

    Add my photo to Navigation sidebar (+ small styles changes)

--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -6,11 +6,13 @@ export default function Navigation() {
   const activePage = usePortfolioStore((state) => state.activePage)
 
   return (
-    <nav className="basis-1/5 p-2 bg-slate-600">
+    <nav className="basis-56 grow-0 bg-slate-700">
+      <img src="/my_photo.jpg" alt="my photo" />
+
       <ul className="content-center text-center uppercase">
-        {pages.map(page => {return <li key={page.name}>
+        {pages.map(page => {return <li key={page.name} className="p-2 border-b border-gray-800">
           <Link 
-            className={page.name === activePage ? "text-slate-50 font-bold hover:text-slate-50" : "text-slate-400 hover:text-slate-300"}
+            className={(page.name === activePage ? "text-slate-50 font-bold hover:text-slate-50" : "text-slate-400 hover:text-slate-300")}
             to={page.path}
           >{page.name}</Link>          
         </li>})}
